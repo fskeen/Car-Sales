7f85a5b13aff0d1ed7c48bc66e910c657e69db90
Faye Skeen <fskeen@gmail.com>
2019-08-14 13:43:11 -0700

    Created store, imported reducer, wrapped Provider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+//redux
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { featureReducer } from './reducers/featureReducer'
+
+//css
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+export const store = createStore(featureReducer)
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , rootElement);
